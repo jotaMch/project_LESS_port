52f2ebecb473a37f6836857071839fc70f2b9ce8
Jânderson Machado <jandersonmachado090@gmail.com>
2023-05-19 00:50:37 -0300

    add new file less

---
+++ main.js
@@ -0,0 +1,19 @@
+const textSub = document.querySelector('.profile-bio-subtitle');
+const text = "Desenvolvedor Front-end"
+const interval = 50;
+
+
+function showText(textSub, text, interval) {
+    const char = text.split("").reverse();
+
+    const type = setInterval(() => {
+        if(!char.length){
+            return clearInterval(type);
+        }
+        const next = char.pop();
+
+        textSub.innerHTML += next;
+    }, interval)
+}
+
+showText(textSub, text, interval);
