8a9b6f4b9840979ba2b3736b8ca64dd13553def6
Jânderson Machado <jandersonmachado090@gmail.com>
2023-05-21 23:08:47 -0300

    alterando bio

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const textSub = document.querySelector('.profile-bio-subtitle');
 const text = "Desenvolvedor Front-end"
-const interval = 50;
-
+const interval = 80;
 
 function showText(textSub, text, interval) {
     const char = text.split("").reverse();
-
     const type = setInterval(() => {
         if(!char.length){
             return clearInterval(type);
