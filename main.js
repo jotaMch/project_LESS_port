fb73e199092d4c6263709115074ff2fcb472b0d6
Jânderson Machado <jandersonmachado090@gmail.com>
2023-06-01 15:55:00 -0300

    new file

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-const textSub = document.querySelector('.profile-bio-subtitle');
-const text = "Desenvolvedor Front-end"
+const textSub = document.querySelector('.subtitle');
+const text = 'Desenvolvedor Front-end';
 const interval = 80;
 
-function showText(textSub, text, interval) {
+function showText() {
     const char = text.split("").reverse();
     const type = setInterval(() => {
-        if(!char.length){
+        if (!char.length) {
             return clearInterval(type);
         }
         const next = char.pop();
-
         textSub.innerHTML += next;
-    }, interval)
+    }, interval);
 }
 
-showText(textSub, text, interval);
+showText();
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
